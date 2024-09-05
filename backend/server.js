f111be5faecfb38d2689b9fb3c05a2ff3fccf8e9
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-05 18:40:34 +0530

    Add : New endpoint to add products in our database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import Product from './models/product.js';
 
 /* 
     Initialize project with node using "npm init -y"
@@ -15,6 +16,37 @@ import { connectDB } from './config/db.js';
 dotenv.config();
 
 const app = express();
+
+
+// Endpoint to create a product for our store
+app.post("/products", async (req, res) => {
+    const product = req.body;           // User will send product details via request body
+    // Check if all the required details are provided in request
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({
+            success: false,
+            message: "Please provide all the details to add a new product."
+        })
+    }
+    // Pass the details to the mongoose model to create a document
+    const newProduct = new Product(product);
+
+    // Save document in database
+    try {
+        newProduct.save();
+        res.status(201).json({
+            success: true,
+            message: newProduct
+        })
+    } catch (error) {
+        console.error("Error in creating product:", error.message);
+        res.status(500).json({
+            success: false,
+            message: "Internal Server Error. Check console for more details."
+        })
+    }
+});
+
 
 app.listen(5000, () => {
     connectDB();
