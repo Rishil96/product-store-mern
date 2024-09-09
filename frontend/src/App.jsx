aa6b01fb565fd22e01f61033a6be67234e3f029e
Rishil Ramesh <rishil.ramesh@outlook.com>
2024-09-09 17:24:24 +0530

    Add : Navbar, HomePage and CreatePage skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { Button } from '@chakra-ui/react'
+import { Box, Button } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <Button>Hello</Button>
-    </>
+    // A Box is a versalite component from Chakra UI that allows us to add styling directly to the component kind of like div from HTML
+    <Box minH={"100vh"}>  
+      {/* NavBar */}
+      <NavBar />
+      {/* Pages */}
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+        <Route path="/create" element={<CreatePage />}/>
+      </Routes>
+    </Box>
   )
 }
 
